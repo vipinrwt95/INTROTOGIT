1ac86b49f44961ea9c81dad1c480d243bcecd12b
vipinrwt95 <vipinrwt9@gmail.com>
2022-10-16 19:26:12 +0530

    adding dom basic file

---
+++ dom.js
@@ -0,0 +1,16 @@
+//examining document object//
+//console.dir(document);
+//console.log(document.domain);
+//console.log(document.URL);
+//console.log(document.title);
+//document.title='Hello World';
+//console.log(document.forms);
+
+let headertitle=document.getElementById('header-title');
+let header=document.getElementById('main-header');
+let title=document.getElementById('main');
+let title2=document.getElementsByClassName("title")
+//console.log(headertitle.textContent);
+//document.getElementById('header-title').innerText="SUPRERMO";
+title2[0].innerHTML='<h2><b>Add Items</b></h2>';
+title2[0].style.color="green";
