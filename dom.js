05e176f3d3af7852bd5667972fb17817f3abd9f2
vipinrwt95 <vipinrwt9@gmail.com>
2022-10-16 20:59:08 +0530

    getelementbytag tutorial

--- dom.js
+++ dom.js
@@ -6,17 +6,22 @@
 //document.title='Hello World';
 //console.log(document.forms);
 
-let headertitle=document.getElementById('header-title');
-let header=document.getElementById('main-header');
-let title=document.getElementById('main');
-let title2=document.getElementsByClassName("title")
+//let headertitle=document.getElementById('header-title');
+//let header=document.getElementById('main-header');
+//let title=document.getElementById('main');
+//let title2=document.getElementsByClassName("title")
 //console.log(headertitle.textContent);
 //document.getElementById('header-title').innerText="SUPRERMO";
-title2[0].innerHTML='<h2><b>Add Items</b></h2>';
-title2[0].style.color="green";
-let items=document.getElementsByClassName("list-group-item")
-items[2].style.backgroundColor='green';
-for(i=0;i<items.length;i++)
-{
-   items[i].style.fontWeight="900"; 
-}
+//title2[0].innerHTML='<h2><b>Add Items</b></h2>';
+//title2[0].style.color="green";
+//let items=document.getElementsByClassName("list-group-item")
+//items[2].style.backgroundColor='green';
+//for(i=0;i<items.length;i++)
+//{
+  // items[i].style.fontWeight="900"; 
+//}
+let item=document.getElementsByTagName("li")
+let itemlist=document.getElementsByClassName("list-group-item")
+console.log(itemlist);
+console.log(itemlist[1])
+item[4].textContent="Changed li";
